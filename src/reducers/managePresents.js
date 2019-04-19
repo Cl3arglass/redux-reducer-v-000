67c53c75fd3fba40fb75e9903175f6ccaf23c386
Holden Altaffer <haltaffer@gmail.com>
2019-04-19 00:34:53 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresentst: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
